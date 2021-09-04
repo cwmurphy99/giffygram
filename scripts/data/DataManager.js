373af1974178955f15dcda235947c897305c2032
Chris Murphy <theworldisyours@gmail.com>
2021-09-04 19:46:11 -0400

    added post details/date/user/description info

--- scripts/data/DataManager.js
+++ scripts/data/DataManager.js
@@ -5,21 +5,32 @@ const loggedInUser = {
     email: "theworldisyours@gmail.com",
     stateId: 35
 }
-
 export const getLoggedInUser = () => {
     return loggedInUser;
 }
-
-
 export const getUsers = () => {
 
     return fetch("http://localhost:8088/users")
         .then(response => response.json())
 }
+// export const getPosts = () => {
+//     return fetch("http://localhost:8088/posts")
+//         .then(response => response.json())
+// }
 
+let postCollection = [];
 
-
+export const usePostCollection = () => {
+  //Best practice: we don't want to alter the original state, so
+  //make a copy of it and then return it
+  //The spread operator makes this quick work
+  return [...postCollection];
+}
 export const getPosts = () => {
-    return fetch("http://localhost:8088/posts")
-        .then(response => response.json())
+  return fetch("http://localhost:8088/posts")
+    .then(response => response.json())
+    .then(parsedResponse => {
+      postCollection = parsedResponse
+      return parsedResponse;
+    })
 }
--- scripts/nav/Footer.js
+++ scripts/nav/Footer.js
@@ -8,10 +8,11 @@ export const Footer = () => {
             </div>
             <div class="footer__item">
                 Posts since <select id="yearSelection">
+                    <option>????</option>
+                    <option>2021</option>
                     <option>2020</option>
                     <option>2019</option>
                     <option>2018</option>
-                    <option>2017</option>
                 </select>
                 <span id="postCount">0</span>
             </div>

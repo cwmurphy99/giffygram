915439c9f15a1c97165d6fdf292f46d52ddf5af2
Chris Murphy <theworldisyours@gmail.com>
2021-09-08 18:46:15 -0400

    css styling, posting capability, filtering option

--- scripts/main.js
+++ scripts/main.js
@@ -1,26 +1,33 @@
 // Can you explain what is being imported here?
-import { getPosts, getUsers, usePostCollection } from "./data/DataManager.js"
+import { getPosts, getUsers, usePostCollection, createPost } from "./data/DataManager.js"
 import { PostList } from "./feed/PostList.js"
 import { NavBar } from "./nav/NavBar.js";
 import { Footer } from "./nav/Footer.js";
+import { PostEntry } from "./feed/PostEntry.js";
 
 const showPostList = () => {
     //Get a reference to the location on the DOM where the list will display
     const postElement = document.querySelector(".postList");
     getPosts().then((allPosts) => {
-        postElement.innerHTML = PostList(allPosts);
+        postElement.innerHTML = PostList(allPosts.reverse());
     })
 }
 const showUserList = () => {
     //Get a reference to the location on the DOM where the list can display
     const postElement = document.querySelector(".userList");
     getUsers().then((allUsers) => {
-        console.log("user info: ", allUsers);
+        //console.log("user info: ", allUsers);
     })
 }
+const showPostEntry = () => { 
+    //Get a reference to the location on the DOM where the nav will display
+    const entryElement = document.querySelector(".entryForm");
+    entryElement.innerHTML = PostEntry();
+  }
 
 const startGiffyGram = () => {
     showNavBar();
+    showPostEntry();
     showPostList();
     showUserList();
     showFooter();
@@ -30,7 +37,7 @@ const applicationElement = document.querySelector(".giffygram");
 
 //EVENT LISTENER FOR 'ON CLICK'
 applicationElement.addEventListener("click", event => {
-    console.log("what was clicked: ", event.target)
+    //console.log("what was clicked: ", event.target)
     //USER CLICKS THE LOGOUT BUTTON => CONFIRMS LOGOUT
     if (event.target.id === "logout") {
         confirm("Are you sure you want to log out?")
@@ -40,8 +47,15 @@ applicationElement.addEventListener("click", event => {
     } else if (event.target.id === "pb-home") {
         //USER CLICKS THE PB ICON => PAGE RELOADS
         window.location.reload();
-    }
+    }else if (event.target.id.startsWith("edit")){
+        //USER CLICKS THE EDIT BUTTON FOR A POST
+		console.log("post clicked", event.target.id.split("--"))
+		console.log("the id is", event.target.id.split("--")[1])
+	}
 })
+
+
+
 
 //NAV BAR INFO
 const showNavBar = () => {
@@ -51,27 +65,37 @@ const showNavBar = () => {
 }
 
 applicationElement.addEventListener("click", event => {
+    if (event.target.id === "newPost__cancel") {
+        //clear the input fields
+    }
+  })
+  applicationElement.addEventListener("click", event => {
     event.preventDefault();
     if (event.target.id === "newPost__submit") {
-        //collect the input values into an object to post to the DB
-        const title = document.querySelector("input[name='postTitle']").value
-        const url = document.querySelector("input[name='postURL']").value
-        const description = document.querySelector("textarea[name='postDescription']").value
-        //we have not created a user yet - for now, we will hard code '1'.
-        //we can add the current time as well
-        const postObject = {
-            title: title,
-            imageURL: url,
-            description: description,
-            userId: 1,
-            timestamp: Date.now()
-        }
-
-        //be sure to import from the DataManager
+    //collect the input values into an object to post to the DB
+      const title = document.querySelector("input[name='postTitle']").value
+      const url = document.querySelector("input[name='postURL']").value
+      const description = document.querySelector("textarea[name='postDescription']").value
+      //we have not created a user yet - for now, we will hard code `1`.
+      //we can add the current time as well
+      const postObject = {
+          title: title,
+          imageURL: url,
+          description: description,
+          userId: 1,
+          timestamp: Date.now()
+      }
+  if(title && url && description){
+    // be sure to import from the DataManager
         createPost(postObject)
-        window.location.reload();
+        .then(dbResponse => {
+            showPostList()
+        });
+    }else{
+        alert("You forgot some info...");
     }
-})
+    }
+  })
 
 //FOOTER INFO
 applicationElement.addEventListener("change", event => {
